9c71233c9ee800edc4c77cab76715949de0970eb
Ckm54 <mutaic84@gmail.com>
2023-07-25 11:59:59 +0300

    add sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,13 +1,5 @@
-import { UserButton } from "@clerk/nextjs";
-import React from "react";
-
 const Dashboard = () => {
-  return (
-    <div>
-      Dashboard
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div>Dashboard</div>;
 };
 
 export default Dashboard;
