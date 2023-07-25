8dd648f3caa521a1a4cbc695c0837a9ebd86c137
Ckm54 <mutaic84@gmail.com>
2023-07-25 12:30:47 +0300

    add mobile navigation

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
+import MobileSidebar from "@/components/Navigation/MobileSidebar";
 
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
       {/* opens mobile sidebar */}
-      <Button variant={"ghost"} size={"icon"} className="md:hidden">
-        <Menu />
-      </Button>
+      <MobileSidebar />
 
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
