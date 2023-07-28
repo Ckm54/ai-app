34f76bdf784d577887b1728154e51770d9c41957
Ckm54 <mutaic84@gmail.com>
2023-07-28 17:26:07 +0300

    :safety_vest: nextauth options

---
+++ lib/authOptions.ts
@@ -0,0 +1,40 @@
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "your_cool_username",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "password",
+        },
+      },
+      async authorize(credentials) {
+        // retrieve credentials from database to verify credentials
+        const user = { id: "21", name: "Collins", password: "xxxxxxx" };
+
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
