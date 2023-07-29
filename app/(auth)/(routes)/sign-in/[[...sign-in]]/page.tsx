9f6520898318e10a8349fa6a207323ae902348b8
Ckm54 <mutaic84@gmail.com>
2023-07-29 20:16:45 +0300

    refactor: update components

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,7 +0,0 @@
-// import { SignIn } from "@clerk/nextjs";
-
-import { Button } from "@/components/ui/button";
-
-export default function Page() {
-  return <Button>Sign in</Button>;
-}
--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,7 +0,0 @@
-// import { SignUp } from "@clerk/nextjs";
-
-import { Button } from "@/components/ui/button";
-
-export default function Page() {
-  return <Button>Sign up</Button>;
-}
