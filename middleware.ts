17052e3017c477cf2becc902a554765efd64de42
Ckm54 <mutaic84@gmail.com>
2023-07-30 14:02:58 +0300

    :hammer: :bug: (fix: ) update auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,47 +1,10 @@
-// import { authMiddleware } from "@clerk/nextjs";
-
-// // This example protects all routes including api/trpc routes
-// // Please edit this to allow other routes to be public as needed.
-// // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
-// export default authMiddleware({
-//   // Landing page is unprotected
-//   publicRoutes: ["/", "/api/webhook"],
-// });
-
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-import { getToken } from "next-auth/jwt";
-import { withAuth } from "next-auth/middleware";
 import { NextRequest, NextResponse } from "next/server";
 
-export default withAuth(
-  // `withAuth` augments your `Request` with the user's token.
-  async function middleware(req: NextRequest) {
-    const pathName = req.nextUrl.pathname;
-    const protectedPaths = ["/", "/dashboard"];
-    const isPathProtected = protectedPaths.some((path) => pathName === path);
-
-    const res = NextResponse.next();
-
-    if (isPathProtected) {
-      const token = await getToken({ req });
-
-      console.log({ token });
-
-      // if (!token) {
-      //   const url = new URL("auth/signin", req.url);
-      //   url.searchParams.set("callbackUrl", pathName);
-      //   return NextResponse.redirect(url);
-      // }
-    }
-    return res;
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => token?.role === "admin",
-    },
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/") {
+    return NextResponse.next();
   }
-);
+  // return NextResponse.redirect(new URL("/auth/signin", request.url));
+}
 
-export const config = { matcher: ["/dashboard"] };
+export const config = { matcher: ["/:path*", "/dashboard"] };
